0723e1e1c3781c7e770f2f8b27315ef0e25da18a
Илья Демьянчик <Neyverlin@outlook.com>
2024-08-16 13:52:57 +0300

    123

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        WordExecuter._targetDirectory = "D:\\work\\Univer\\Task 1 - Comments of modules (read word and paste into excel)\\Каталог учебных дисцилин";
-        //WordExecuter.ProcessRootDirectoryToFindOtherFoldersWithFiles();
-        //ModuleService.PrintAllErrorDocsInDatabase();
+        WordExecuter._targetDirectory = "D:\\Ilya\\2024\\08\\Project\\Каталог учебных дисцилин";
+        WordExecuter.ProcessRootDirectoryToFindOtherFoldersWithFiles();
+        ////ModuleService.PrintAllErrorDocsInDatabase();
 
 
-        var translator = new GTranslatorAPI.GTranslatorAPIClient();
-        var result = await translator.TranslateAsync(GTranslatorAPI.Languages.be, GTranslatorAPI.Languages.ru, "Практычная і функцыянальная стылістыка беларускай мовы");
-        System.Console.WriteLine(result.TranslatedText);
+        //var translator = new GTranslatorAPI.GTranslatorAPIClient();
+        //var result = await translator.TranslateAsync(GTranslatorAPI.Languages.be, GTranslatorAPI.Languages.ru, "Практычная і функцыянальная стылістыка беларускай мовы");
+        //System.Console.WriteLine(result.TranslatedText);
 
     }
-    
+
 }
