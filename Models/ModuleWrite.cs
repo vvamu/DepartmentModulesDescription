e3b4d1822c9cc294a819d8d0274e6666d35ae97f
Bichun Nadezhda <nadezhda.bn.02@mail.ru>
2024-08-30 11:48:13 +0300

    fix problem with 0 ауд.ч

--- Models/ModuleWrite.cs
+++ Models/ModuleWrite.cs
@@ -10,12 +10,12 @@ public class ModuleWrite
 
     public string Receives { get; set; } = string.Empty;
     public string TotalHours { get; set; } = string.Empty;
-    public string AuditoriumHours { get; set; } = string.Empty; public string GetAuditoriumHours => AuditoriumHours == null ? "0" : AuditoriumHours + " ауд. ч.";
-    public string LectureHours { get; set; } = string.Empty; public string GetLectureHours => LectureHours == null ? "0" : LectureHours + " лекционных ч.";
-    public string LabsHours { get; set; } = string.Empty; public string GetLabsHours => LabsHours == null ? "0" : LabsHours + "лаб. ч.";
+    public string AuditoriumHours { get; set; } = string.Empty; public string GetAuditoriumHours => AuditoriumHours == null ? "0  ауд. ч." : AuditoriumHours + " ауд. ч.";
+    public string LectureHours { get; set; } = string.Empty; public string GetLectureHours => LectureHours == null ? "0 лекционных ч." : LectureHours + " лекционных ч.";
+    public string LabsHours { get; set; } = string.Empty; public string GetLabsHours => LabsHours == null ? "0 лаб. ч." : LabsHours + " лаб. ч.";
 
-    public string PracticeHours { get; set; } = string.Empty; public string GetPracticeHours => PracticeHours == null ? "0" : PracticeHours + " практических ч.";
-    public string SeminarHours { get; set; } = string.Empty; public string GetSeminarHours => SeminarHours == null ? "0" : SeminarHours + " семинарских ч.";
+    public string PracticeHours { get; set; } = string.Empty; public string GetPracticeHours => PracticeHours == null ? "0  практических ч." : PracticeHours + " практических ч.";
+    public string SeminarHours { get; set; } = string.Empty; public string GetSeminarHours => SeminarHours == null ? "0 семинарских ч." : SeminarHours + " семинарских ч.";
     public string DepartmentShortName { get; set; } = string.Empty;
 
     public string ReceivedUnits { get; set; } = string.Empty;
